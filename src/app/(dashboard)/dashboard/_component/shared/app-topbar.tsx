cb7a37e4744a1f42d5d648516c6656dd6a9df406
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-10-29 15:26:28 +0600

    treatment table done

--- src/app/(dashboard)/dashboard/_component/shared/app-topbar.tsx
+++ src/app/(dashboard)/dashboard/_component/shared/app-topbar.tsx
@@ -1,10 +1,22 @@
+"use client";
 import React from "react";
 import PathTracker from "./path-tracker";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export const AppTopBar = () => {
+  const pathName = usePathname();
+
   return (
-    <div className="p-5 rounded-lg shadow-[0px_0px_1px_1px_#0000001A]">
+    <div className="p-5 rounded-lg shadow-[0px_0px_1px_1px_#0000001A] flex items-center justify-between">
       <PathTracker />
+
+      {pathName === "/dashboard/treatment-list" && (
+        <Button className="h-[40px]">
+          <Plus /> Add Service
+        </Button>
+      )}
     </div>
   );
 };
