2a285af26e0ce1993056e4c8cb91b6dcde681f07
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-10-31 15:43:19 +0600

    done

--- src/components/common/treatment-cart.tsx
+++ src/components/common/treatment-cart.tsx
@@ -10,13 +10,13 @@ const TreatmentCart = ({ item }: { item: TreatmentGroup }) => {
         // src={
         //   item?.treatments[0]?.image || "/assets/images/no-image.jpg"
         // }
-        alt={item?.treatments[0]?.serviceName || ""}
+        alt={item?.treatments?.[0]?.serviceName || ""}
         width={1000}
         height={1000}
         className="w-full h-[431px] rounded-[8px] object-cover"
       />
       <h3 className="text-base md:text-lg font-medium leading-[120%] text-[#2F2F2F] text-center bg-white rounded-b-[8px] py-2 md:py-3">
-        {item?.treatments[0]?.serviceName}
+        {item?.treatments?.[0]?.serviceName}
       </h3>
     </div>
   );
