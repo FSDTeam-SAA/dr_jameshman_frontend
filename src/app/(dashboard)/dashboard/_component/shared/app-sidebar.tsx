8419716b8b07f1d684c1c9148bb52d0844d9d92f
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-11-04 16:10:50 +0600

    add logout feature in dashboard

--- src/app/(dashboard)/dashboard/_component/shared/app-sidebar.tsx
+++ src/app/(dashboard)/dashboard/_component/shared/app-sidebar.tsx
@@ -5,6 +5,7 @@ import {
   DollarSign,
   FileImage,
   List,
+  LogOut,
   MessageSquareText,
   NotebookPen,
   RefreshCcwDot,
@@ -14,6 +15,7 @@ import {
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -24,6 +26,7 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { signOut } from "next-auth/react";
 
 // Menu items.
 const items = [
@@ -77,48 +80,69 @@ const items = [
 export function AppSidebar() {
   const pathName = usePathname();
 
+  const handleLogout = () => {
+    signOut({
+      callbackUrl: "/login",
+    });
+  };
+
   return (
     <Sidebar className="border-none">
       <SidebarContent className="bg-white shadow-2xl">
         <SidebarGroup className="p-0">
-          <SidebarGroupLabel className="mt-5 mb-5 h-[80px]">
-            <Link href={`/`}>
-              <Image
-                src={`/assets/images/dashboard-logo.png`}
-                alt="logo.png"
-                width={1000}
-                height={1000}
-                className="h-[200px] w-[218px] object-cover"
-              />
-            </Link>
-          </SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => {
-                const isActive =
-                  item.url === "/dashboard"
-                    ? pathName === "/dashboard"
-                    : pathName === item.url ||
-                      pathName.startsWith(`${item.url}/`);
+          <div className="flex flex-col justify-between min-h-screen pb-5">
+            <div>
+              <SidebarGroupLabel className="mt-5 mb-5 h-[80px]">
+                <Link href={`/`}>
+                  <Image
+                    src={`/assets/images/dashboard-logo.png`}
+                    alt="logo.png"
+                    width={1000}
+                    height={1000}
+                    className="h-[200px] w-[218px] object-cover"
+                  />
+                </Link>
+              </SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {items.map((item) => {
+                    const isActive =
+                      item.url === "/dashboard"
+                        ? pathName === "/dashboard"
+                        : pathName === item.url ||
+                          pathName.startsWith(`${item.url}/`);
 
-                return (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton
-                      className={`h-[60px] rounded-none text-[20px] hover:bg-primary hover:text-white transition-all duration-300 pl-5 ${
-                        isActive && "bg-primary text-white font-medium"
-                      }`}
-                      asChild
-                    >
-                      <Link href={item.url}>
-                        <item.icon />
-                        <span>{item.title}</span>
-                      </Link>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                );
-              })}
-            </SidebarMenu>
-          </SidebarGroupContent>
+                    return (
+                      <SidebarMenuItem key={item.title}>
+                        <SidebarMenuButton
+                          className={`h-[60px] rounded-none text-[20px] hover:bg-primary hover:text-white transition-all duration-300 pl-5 ${
+                            isActive && "bg-primary text-white font-medium"
+                          }`}
+                          asChild
+                        >
+                          <Link href={item.url}>
+                            <item.icon />
+                            <span>{item.title}</span>
+                          </Link>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    );
+                  })}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </div>
+
+            <div>
+              <SidebarFooter className="border-t border-gray-300">
+                <button
+                  onClick={handleLogout}
+                  className="font-medium text-red-500 flex items-center gap-2 pl-2 mt-5"
+                >
+                  <LogOut className="h-4 w-4" /> Log out
+                </button>
+              </SidebarFooter>
+            </div>
+          </div>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
